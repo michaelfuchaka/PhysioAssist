2d093698af61e7eda6cc6198df8efe2cbc56a361
michaelfuchaka <fuchakamichael06@gmail.com>
2025-12-29 15:49:08 +0300

    feat: Implement New Case form with navigation; enhance user input experience and layout

--- physioAssist/client/src/app/(protected)/newcase/page.jsx
+++ physioAssist/client/src/app/(protected)/newcase/page.jsx
@@ -1,12 +1,53 @@
 import React from 'react'
 import Sidebar from "@/components/Sidebar"; 
+import { ChevronRight } from 'lucide-react';
+import Link from 'next/link';
 
 
 const NewCase = () => {
   return (
     <div> 
       <Sidebar />
-       </div>
+       <main className="ml-60 p-4 bg-[#F5F5F5] text-[#000000] min-h-screen">
+        <div className='ml-4'>
+          <nav>
+        <div className="flex items-center gap-2 text-lg font-semibold">
+            <Link href="/dashboard">Dashboard</Link>
+            <ChevronRight size={24} className="" />
+            <Link href="/newcase">New Case</Link>
+        </div>
+        </nav>
+        {/* Page Header */}
+        <div className='mt-12 bg-white shadow-sm max-w-lg mx-auto p-6 rounded-xl'> 
+          <h2 className='text-xl font-bold mb-4 text-center'>
+            New Patients Symptom Analysis
+          </h2>
+              {/* Form Inputs */}
+          <form className='space-y-4'>
+            <input type="text" placeholder="Pain Region" required className=' w-full px-4 py-3  bg-[#F5F5F5]
+             rounded-xl  border border-[#324B6F] focus:outline-none  focus:border-[#324B6F]' />
+          <input type="text" placeholder="Symptoms" required className=' w-full px-4 py-3  bg-[#F5F5F5]
+             rounded-xl  border border-[#324B6F] focus:outline-none  focus:border-[#324B6F]' />
+          <input type="text" placeholder="Duration " required className=' w-full px-4 py-3  bg-[#F5F5F5]
+             rounded-xl  border border-[#324B6F] focus:outline-none  focus:border-[#324B6F]' />
+           <input type="text" placeholder="Aggravating Factors (Optional)" className=' w-full px-4 py-3  bg-[#F5F5F5]
+             rounded-xl  border border-[#324B6F] focus:outline-none  focus:border-[#324B6F]' />
+          <input type="text" placeholder="Additional Information(Optional) "  className=' w-full px-4 py-3  bg-[#F5F5F5]
+             rounded-xl  border border-[#324B6F] focus:outline-none  focus:border-[#324B6F]' />
+         <div className='flex gap-4 mt-6'>
+         <button className="flex-1 bg-[#E0E0E0] font-semibold py-3 rounded-lg hover:bg-[#D0D0D0] transition-colors">
+          Save Draft
+          </button>
+         <button className="flex-1 bg-[#324B6F] text-white font-semibold py-3 rounded-lg hover:bg-slate-700 transition-colors">
+          Analyze Symptoms
+          </button>
+          </div>
+         </form>
+        </div>
+        </div>
+      </main>  
+    
+    </div>
   )
 }
 
