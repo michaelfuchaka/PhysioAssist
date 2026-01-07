5ee96150e6dfac9e95edbe0374ca7478f864ef13
michaelfuchaka <fuchakamichael06@gmail.com>
2026-01-07 11:34:11 +0300

    feat: implement 2-column layout for Results page

--- physioAssist/client/src/app/(protected)/results/page.jsx
+++ physioAssist/client/src/app/(protected)/results/page.jsx
@@ -17,15 +17,29 @@ const results = () => {
        
         </div>
         </nav>
-        <div className='mt-12 '>
-        <div className='bg-[#F5F5F5] shadow-sm max-w-lg p-6 rounded-xl'>
+        <div className='mt-12 grid grid-cols-1 md:grid-cols-2 gap-4" '>
+          {/* possible conditions */}
+        <div className='bg-[#F5F5F5] shadow-sm max-w-lg p-6 rounded-xl '>
             <h2 className='font-semibold text-[#324B6F] text-xl'>
                 Possible Conditions
             </h2>
-
-           <button className="flex-1 bg-[#E0E0E0] font-medium py-4 rounded-lg hover:bg-[#D0D0D0] transition-colors">
-          Reject All & Re-analyze
+          <button className="flex-1 bg-[#E0E0E0] font-medium py-2 px-8 rounded-lg hover:bg-[#D0D0D0] transition-colors whitespace-normal">
+            Reject All & Re-analyze
           </button>
+        </div>
+           <div className='flex flex-col gap-4'>
+        {/* Recommended treatment plan  */}
+        <div  className='bg-[#F5F5F5] shadow-sm max-w-lg p-6 rounded-xl'>
+            <h2 className='font-semibold text-[#324B6F] text-xl'>
+                Recommended Treatment Plan
+            </h2>
+        </div>
+        {/* Clinical documentation */}
+         <div  className='bg-[#F5F5F5] shadow-sm max-w-lg p-6 rounded-xl'>
+            <h2 className='font-semibold text-[#324B6F] text-xl'>
+                Clinical Documentation
+            </h2>
+        </div>
         </div>
         </div>
         </div>
